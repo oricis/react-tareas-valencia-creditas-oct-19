96ddab042d01b2d69ee65154111fbc72b66bb108
jupegarnica <jupegarnica@gmail.com>
2019-10-15 12:58:05 +0200

    add task working

--- src/components/Tareas.jsx
+++ src/components/Tareas.jsx
@@ -1,39 +1,67 @@
 import React from 'react';
 import './Tareas.css';
-import Tarea from './Tarea.jsx'
+import Tarea from './Tarea.jsx';
 import generateID from '../services/generateID.js';
 
-
 class Tareas extends React.Component {
-  constructor (props) {
-     super(props);
-     this.state = {
-       tasks: [
-         {
-           text: 'aprender react',
-           completed: false,
-           id: generateID(),
-         },
-         {
-           text: 'aprender jsx',
-           completed: true,
-           id: generateID(),
-         },
-       ],
-     };
+  constructor(props) {
+    super(props);
+    this.state = {
+      newTaskText: '',
+      tasks: [
+        {
+          text: 'aprender react',
+          completed: false,
+          id: generateID(),
+        },
+      ],
+    };
   }
+  addTask(text) {
+    text = text.trim();
+    if (text) {
+      const newTask = {
+        text: text,
+        completed: false,
+        id: generateID(),
+      };
 
-  render () {
-     return (
-       <div className='Tareas'>
-         <section className='addTask'>
-           <input type='text' placeholder='añade tu tarea aquí' />
-         </section>
-         <main className='taskList'>
-              {this.state.tasks.map(task => <Tarea key={task.id} data={task}/>)}
-         </main>
-       </div>
-     );
+      this.setState({
+        newTaskText: '',
+        tasks: [...this.state.tasks, newTask],
+      });
+    }
+  }
+  handleKeyPress = event => {
+    if (event.key === 'Enter') {
+      this.addTask(this.state.newTaskText);
+    }
+  };
+
+  render() {
+    return (
+      <div className='Tareas'>
+        <section className='addTask'>
+          <input
+            type='text'
+            placeholder='añade tu tarea aquí'
+            value={this.state.newTaskText}
+            onKeyPress={this.handleKeyPress}
+            onChange={event =>
+              this.setState({ newTaskText: event.target.value })
+            }
+          />
+          <button onClick={() => this.setState({ newTaskText: '' })}>
+            clean
+          </button>
+        </section>
+        <main className='taskList'>
+          {this.state.tasks.map(task => (
+            <Tarea key={task.id} data={task} />
+          ))}
+        </main>
+      </div>
+    );
   }
 }
 
