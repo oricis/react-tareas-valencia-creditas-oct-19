a354c316de84859c7e40e84bbec2387520799e6a
jupegarnica <jupegarnica@gmail.com>
2019-10-16 13:31:08 +0200

    remove header

--- src/App.js
+++ src/App.js
@@ -5,9 +5,7 @@ import Tareas from './components/Tareas.jsx';
 function App() {
   return (
     <div className="App">
-      <header>
-        <h3>Cosas que hacer antes de morir</h3>
-      </header>
+
       <Tareas></Tareas>
 
     </div>
